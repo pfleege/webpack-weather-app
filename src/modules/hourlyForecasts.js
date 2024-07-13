2d5035d632cd74b34d50ea029799d582a52cd7de
Fleege <76089935+pfleege@users.noreply.github.com>
2024-07-13 10:41:49 +0300

    ADD footnote module and background to project
    - Add new icsDemo module to project and include it via hourly module
    - Add background image and update CSS

--- src/modules/hourlyForecasts.js
+++ src/modules/hourlyForecasts.js
@@ -1,3 +1,4 @@
+import icsStatement from "./icsDemo";
 const container = document.querySelector("#pageContent");
 
 function hourlyForecasts(json) {
@@ -10,26 +11,27 @@ function hourlyForecasts(json) {
   for (let i = 0; i < 24; i = i + 3) {
     let dayContainer = document.createElement("div");
     let hour = document.createElement("div");
-    let icon = document.createElement("img");
+    let weatherIcon = document.createElement("img");
     let temp = document.createElement("div");
 
     dayContainer.className = `hour${[i]}Container`;
     hour.className = `hour${[i]}`;
 
     hour.textContent = json.forecast.forecastday[0].hour[i].time;
-    icon.className = `hourIcon`;
-    icon.src = json.forecast.forecastday[0].hour[i].condition.icon;
+    weatherIcon.className = `hourIcon`;
+    weatherIcon.src = json.forecast.forecastday[0].hour[i].condition.icon;
     temp.className = `hour${[i]}Temp`;
     temp.textContent = `${json.forecast.forecastday[0].hour[i].temp_c}°C`;
 
     dayContainer.appendChild(hour);
-    dayContainer.appendChild(icon);
+    dayContainer.appendChild(weatherIcon);
     dayContainer.appendChild(temp);
     hourlyForecastContent.appendChild(dayContainer);
   }
 
   hourlyForecastContainer.appendChild(hourlyForecastContent);
   container.appendChild(hourlyForecastContainer);
+  container.appendChild(icsStatement());
 }
 
 export default hourlyForecasts;
---
+++ src/modules/icsDemo.js
@@ -0,0 +1,25 @@
+import icsLogo from "../img/iCeeqSolutionsLogo.png";
+
+function icsStatement() {
+  const container = document.createElement("div");
+  const p = document.createElement("p");
+  const a = document.createElement("a");
+  const img = document.createElement("img");
+  /* const logo = new Image(); */
+
+  container.className = "ics-container";
+  p.className = "ics-p";
+  p.textContent = "Webpack demo by: ";
+  a.href = "https://iceeqsolutions.fi";
+  a.target = "_blank";
+  img.className = "ics-img";
+  img.src = icsLogo;
+
+  a.appendChild(img);
+  container.appendChild(p);
+  container.appendChild(a);
+
+  return container;
+}
+
+export default icsStatement;
